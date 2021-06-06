aba75733c672ffa8bc2ded33345b533cb5d0ed5d
Fauzan482 <79176630+Fauzan482@users.noreply.github.com>
2021-06-07 05:13:41 +0700

    Add files via upload

---
+++ logist/home.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('location:login.php');
+}
+
+?>
+
+<html>
+
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <a class="float-right" href="logout.php">LOGOUT</a>
+        <h1> Wellcome <?php echo $_SESSION['username']; ?> </h1>
+    </div>
+</body>
+
+</html>
---
+++ logist/login.php
@@ -0,0 +1,62 @@
+<html>
+
+<head>
+    <title>User Login and Registration</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    
+</head>
+
+<body>
+    <div class="container">
+        <div class="login-box">
+            <div class="gambar" id="gambar">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Login Here</h2>
+                    <form action="validation.php" method="post">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="user" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+
+                <div class="col-md-6">
+                    <h2>Register Here</h2>
+                    <form action="registration.php" method="post">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="user" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                        
+                    </form>
+                    <form action="home.html">
+                        <button onclick="goBack()">Kembali</button>
+                        <script>
+                            function goBack() {
+                            window.history.back();
+                            }
+                        </script> 
+                    </form>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</body>
+
+</html>
